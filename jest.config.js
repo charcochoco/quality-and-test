3e93c2a731151e5f7e803da0a6c1739f4e579118
charcochoco <romain.charcosset08@gmail.com>
2024-12-17 09:54:50 +0100

    badge

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
     testMatch: ['**/tests/jest/**/*.test.js'], // Limite aux tests dans `jest/`
     collectCoverage: true,
     coverageDirectory: './coverage',
+    coverageReporters: ["json-summary"]
 };
